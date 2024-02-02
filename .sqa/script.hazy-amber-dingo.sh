b0f2031bbf2a7061d892a13ff10cd50b94ebe265
orviz <orviz@ifca.unican.es>
2024-02-02 11:08:24 +0100

    Add JePL file structure

---
+++ .sqa/script.hazy-amber-dingo.sh
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd gitlab.in2p3.fr/ipsl/espri/espri-mod/intertwin &&
+    find_doc_files.py --file_type all
+)
