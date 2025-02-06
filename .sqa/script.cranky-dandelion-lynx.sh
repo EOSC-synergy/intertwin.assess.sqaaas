723f6f877c6022ad098725e82d8280dc9736ca92
Pablo Orviz <orviz@ifca.unican.es>
2025-02-06 11:51:10 +0100

    Add JePL file structure

--- .sqa/script.cranky-dandelion-lynx.sh
+++ .sqa/script.cranky-dandelion-lynx.sh
@@ -1,8 +1,9 @@
 # SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
 (
 cd gitlab.in2p3.fr/ipsl/espri/espri-mod/intertwin &&
-    find_doc_files.py --file_type all
+    mdl --json  .
 )
